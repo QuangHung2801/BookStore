8fdbda606dba65360c9d4d54ac2082d692ebc44c
HungGau <kabutozex@gmail.com>
2024-07-06 15:04:57 +0700

    tuanhung
    guibai

--- src/main/java/com/example/WebBanSach/services/ProductService.java
+++ src/main/java/com/example/WebBanSach/services/ProductService.java
@@ -4,10 +4,8 @@ import com.example.WebBanSach.entity.Product;
 import com.example.WebBanSach.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -40,19 +38,26 @@ public class ProductService {
     public Page<Product> getAllProducts(Pageable pageable) {
         return productRepository.findAll(pageable);
     }
+
     public void saveAll(List<Product> products) {
         productRepository.saveAll(products);
     }
+
     public void deleteProductsByIds(List<Long> productIds) {
         for (Long id : productIds) {
             productRepository.deleteById(id);
         }
     }
+
     public Product getProductByTitle(String title) {
         return productRepository.findByTitle(title);
     }
+
     public Page<Product> searchProductsByTitle(String title, Pageable pageable) {
         return productRepository.findByTitleContaining(title, pageable);
     }
 
+    public List<Product> findRelatedProducts(Long categoryId, Long productId) {
+        return productRepository.findByCategoryIdAndIdNot(categoryId, productId);
+    }
 }
